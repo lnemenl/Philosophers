3e726970529492ecd2ce83b2251cb072efdf7da3
Ruslan Khakimullin <ruslankhakimullin@gmail.com>
2024-11-12 12:08:09 +0200

    reorganization of the structs in the header file

--- inc/philosophers.h
+++ inc/philosophers.h
@@ -6,28 +6,40 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 23:49:30 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/11/10 23:56:36 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/11/12 12:07:37 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
 
 #include <pthread.h>
 #include <sys/time.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct  s_control
+typedef struct s_control
 {
-    int             *turn_counter;
-    pthread_mutex_t *turn_mutex;
-    int             number_of_philosophers;
-}                   t_control;
+	int				number_of_philosophers;
+	int				active_group;
+	pthread_mutex_t	group_mutex;
+	pthread_mutex_t	*forks;
+}					t_control;
 
-typedef struct  s_philosopher
+typedef struct s_philosopher
 {
-    int             id;
-    long            last_meal_time;
-    pthread_mutex_t *left_fork;
-    pthread_mutex_t *right_fork;
-    t_control       *control;
-}                   t_philosopher;
+	int				id;
+	long			last_meal_time;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	t_control		*control;
+}					t_philosopher;
 
+typedef struct s_simulation
+{
+	t_control		*control;
+	t_philosopher	*philosopher;
+}					t_simulation;
+
+
+#endif
