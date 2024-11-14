3473c90d8832ea21117db0cf4e83aea5caabc951
Ruslan Khakimullin <ruslankhakimullin@gmail.com>
2024-11-14 15:25:52 +0200

    added main function

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,35 @@
 /*   By: rkhakimu <rkhakimu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 15:08:49 by rkhakimu          #+#    #+#             */
-/*   Updated: 2024/11/14 15:08:50 by rkhakimu         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:24:33 by rkhakimu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philosophers.h"
+
+int	main(int ac, char **av)
+{
+	t_control		control;
+	t_simulation	simulation;
+	int				result;
+	
+	if (parse_arguments(ac, av, &control) != 0)
+		return (1);
+	simulation.control = initialize_control(control.number_of_philosophers);
+	if (!simulation.control)
+		return (1);
+	simulation.philosophers = malloc(sizeof(t_philosopher) * control.number_of_philosophers);
+	if (!simulation.philosophers)
+	{
+		free;
+		return(1);
+	}
+	if (initialize_philosophers(simulation.philosophers, control.number_of_philosophers, simulation.control) != 0)
+	{
+		somecleanup;
+		return (1);
+	}
+	result = start_philosophers(&simulation);
+	somecleanup;
+	return (result);
+}
